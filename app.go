d81c72309d09947ed1b5472e9e0fa7aa42277e93
dengyinsen <dengyinsen@gutou.com>
2020-04-24 15:12:38 +0800

    gin中间件

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/ebar-go/ego/http"
+	"github.com/ebar-go/ego/http/middleware"
 	"github.com/ebar-go/ego/http/response"
 	"github.com/ebar-go/ego/utils"
 	"github.com/gin-gonic/gin"
@@ -15,5 +16,8 @@ func main() {
 		}
 		response.WrapContext(context).Success(data)
 	})
+	server.Router.Use(middleware.CORS)
+	server.Router.Use(middleware.RequestLog)
+	server.Router.Use(middleware.Recover)
 	utils.FatalError("StartServer", server.Start())
 }
