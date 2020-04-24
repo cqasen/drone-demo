85e0147f8d9a96c4b8a73a382cfdf5a2324e7abd
dengyinsen <dengyinsen@gutou.com>
2020-04-24 15:09:59 +0800

    gin

--- app.go
+++ app.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ebar-go/ego/http"
+	"github.com/ebar-go/ego/http/response"
+	"github.com/ebar-go/ego/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("---------------------------")
-	fmt.Println("Hello World!")
+	server := http.NewServer()
+	server.Router.GET("/", func(context *gin.Context) {
+		data := response.Data{
+			"tips": "Hello World!",
+		}
+		response.WrapContext(context).Success(data)
+	})
+	utils.FatalError("StartServer", server.Start())
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/cqasen/drone-demo
-
-go 1.12
