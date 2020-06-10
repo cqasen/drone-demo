1c69d88ee53e43dd13045045fb5237b5447f2341
dengyinsen <dengyinsen@gutou.com>
2020-06-10 09:29:59 +0800

    路由分组

--- http/router/router.go
+++ http/router/router.go
@@ -7,12 +7,20 @@ import (
 )
 
 func InitRouter(router *gin.Engine) {
+	//中间件
 	router.Use(middleware.CORS)
 	router.Use(middleware.RequestLog)
 	router.Use(middleware.Recover)
+	router.Use(middleware.Favicon)
+	//资源
 	router.StaticFile("favicon.ico", "./resources/favicon.ico")
-
+	//路由
 	router.GET("/", handler.IndexHandler)
-	router.GET("/post/:id", handler.GetPost)
-	router.GET("/post", handler.GetPostList)
+	//router.GET("/post/:id", handler.GetPost)
+	//router.GET("/post", handler.GetPostList)
+	post := router.Group("/post")
+	{
+		post.GET("/:id", handler.GetPost)
+		post.GET("", handler.GetPostList)
+	}
 }
