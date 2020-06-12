f24833f9667bb043b7eb31e8f10e95d13ae027ff
dengyinsen <dengyinsen@gutou.com>
2020-06-12 10:03:36 +0800

    更新

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"github.com/casbin/casbin"
 	"github.com/cqasen/gin-demo/http/router"
-	app2 "github.com/cqasen/gin-demo/pkg/app"
 	"github.com/cqasen/gin-demo/pkg/config"
 	"github.com/ebar-go/ego/app"
 	"github.com/ebar-go/ego/http"
@@ -18,14 +19,25 @@ func main() {
 	}()
 	//加载配置
 	config.InitConfig()
+	//加载权限
+	mycasbin()
+
 	//获取http服务对象
 	server := http.NewServer()
 	secure.FatalError("Mysql Start", app.InitDB())
 	secure.FatalError("Redis Start", app.InitRedis())
 	//链接es
-	secure.FatalError("Elasticsearch Start", app2.InitElasticsearch())
+	//secure.FatalError("Elasticsearch Start", app2.InitElasticsearch())
 	//加载路由
 	router.InitRouter(server.Router)
 	//启动服务
 	secure.FatalError("StartServer", server.Start())
+}
+
+func mycasbin()  {
+	log.Println("加载权限配置BEGIN")
+	//e, _:= casbin.NewEnforcer("path/to/model.conf", "path/to/policy.csv")
+	e := casbin.NewEnforcer("./config/rbac_model.conf", "./config/rbac_policy.csv")
+	fmt.Println(e)
+	log.Println("加载权限配置END")
 }
--- pkg/model/dao/post.go
+++ pkg/model/dao/post.go
@@ -16,7 +16,7 @@ func Post(db *gorm.DB) *PostDao {
 
 func (dao PostDao) Get(id int32) (*entity.ZbpPost, error) {
 	post := new(entity.ZbpPost)
-	query := dao.db.Where(entity.ZbpPost{LogID: id}).First(post)
+	query := dao.db.Table(entity.TABLE_POST).Debug().Where(entity.ZbpPost{LogID: id}).First(post)
 	if err := query.Error; err != nil {
 		return nil, err
 	}
